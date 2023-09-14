de50f5948143f1bf5fb14abf1cc744880d2651b5
Sofia <sofiamino78@gmail.com>
2023-09-14 09:50:16 -0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,42 @@
+import React from "react";
 import logo from './logo.svg';
+import Componente from "./components/Componente";
+import Propiedades from "./components/Propiedades";
+import Estado from "./components/Estado";
+import RenderizadoCondicional from "./components/RenderizadoCondicional";
+import RenderizadoElementos from "./components/RenderizadoElementos";
+import {EventosES6, EventosES7, MasSobreEventos} from "./components/Eventos";
+import ComunicacionComponentes from "./components/ComunicacionComponentes";
+import CicloVida from "./components/CicloVida";
+import AjaxApis from "./components/AjaxApis";
+import ContadorHooks from "./components/ContadorHooks";
+import ScrollHooks from "./components/ScrollHooks";
+import RelojHooks from "./components/RelojHooks";
+//import AjaxHooks from "./components/AjaxHooks";
+import HooksPersonalizados from "./components/HooksPersonalizados";
+import Referencias from "./components/Referencias";
+import Formularios from "./components/Formularios";
 import './App.css';
+import Estilos from "./components/Estilos";
+import ComponentesEstilizados from "./components/ComponentesEstilizados";
+
+
+
+
+
+
+
+
+
+
+
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <section>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,6 +49,68 @@ function App() {
         >
           Learn React
         </a>
+        </section>
+
+        <section>
+        <Componente msg="Hola soy un Componente Funcional  Expresado desde una prop"/>
+        <hr/>
+        <Propiedades 
+        cadena="Esto es una cadena de texto" 
+        numero={19} 
+        booleano={true}
+        arreglo={[1,2,3]}
+        objeto={{nombre:"Sofia", correo:"sofiamino78@gmail.com"}}
+        funcion={(num) => num*num}
+        elementoReact={<i>Esto es un elemento React</i>}
+        componenteReact={<Componente msg="Soy un componente pasado como Prop"/>}
+        />
+        <hr/>
+        <Estado/>
+        <hr/>
+        <RenderizadoCondicional/>
+        <hr/>
+        <RenderizadoElementos/>
+        <hr/>
+        <EventosES6/>
+        <hr/>
+        <EventosES7/>
+        <hr/>
+        <MasSobreEventos/>
+        <hr/>
+        <ComunicacionComponentes/>
+        <hr/>
+        <CicloVida/>
+        <hr/>
+        <AjaxApis/>
+        <hr/>
+        <ContadorHooks/>
+        <hr/>
+        <ScrollHooks/>
+        <hr/>
+        <RelojHooks/>
+        <hr/>
+        {/* <AjaxHooks/>
+        <hr/> */}
+        {/* <HooksPersonalizados/>
+        <hr/> */}
+        <Referencias/>
+        <hr/>
+        <Formularios/>
+        <hr/>
+        <Estilos/>
+        <hr/>
+        <ComponentesEstilizados/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        </section>
       </header>
     </div>
   );
